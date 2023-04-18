855db5bb87a37b6da2c1f75a26677cb22ba8ac1d
adimdhifallah <131157552+adimdhifallah@users.noreply.github.com>
2023-04-18 14:31:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,23 +35,3 @@ function calculateBMI() {
 
   showResult(BMI);
 }
-
-const displayBMI = document.querySelector(".bmi-value");
-const result = document.querySelector(".result");
-
-function handleError() {
-  displayBMI.textContent = "Wops";
-  displayBMI.style.color = "inherit";
-  result.textContent = "Remplissez correctement les inputs.";
-}
-
-function showResult(BMI) {
-  const rank = BMIData.find(data => {
-    if (BMI >= data.range[0] && BMI < data.range[1]) return data;
-    else if (typeof data.range === "number" && BMI >= data.range) return data;
-  });
-
-  displayBMI.textContent = BMI;
-  displayBMI.style.color = `${rank.color}`;
-  result.textContent = `Résultat : ${rank.name}`;
-}
